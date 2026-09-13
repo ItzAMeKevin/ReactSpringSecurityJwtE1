54974de1c76fc709cb4c9de344eaf86447582efd
IulianBlyat <iulianghitiu0102@gmail.com>
2026-09-12 20:13:23 -0400

    docker implimentation

--- src/main/java/com/lacouf/rsbjwt/ReactSpringSecurityJwtApplication.java
+++ src/main/java/com/lacouf/rsbjwt/ReactSpringSecurityJwtApplication.java
@@ -10,7 +10,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.time.LocalDate;
 import java.util.Optional;
 
 @SpringBootApplication
@@ -37,30 +36,37 @@ public class ReactSpringSecurityJwtApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        gestionnaireRepository.save(
+        if (userAppRepository.findUserAppByEmail("l@l.com").isEmpty()) {
+            gestionnaireRepository.save(
                 SystemManager.builder()
-                        .firstName("Gerard")
-                        .lastName("Biblio")
-                        .email("l@l.com")
-                        .password(passwordEncoder.encode("bib"))
-                        .build()
-        );
-        emprunteurRepository.save(
+                    .firstName("Gerard")
+                    .lastName("Biblio")
+                    .email("l@l.com")
+                    .password(passwordEncoder.encode("bib"))
+                    .build()
+            );
+        }
+        if (userAppRepository.findUserAppByEmail("ll@l.com").isEmpty()) {
+            emprunteurRepository.save(
                 Employer.builder()
-                        .firstName("Isidor")
-                        .lastName("Teurteur")
-                        .email("ll@l.com")
-                        .password(passwordEncoder.encode("bib"))
-                        .build()
-        );
-        preposeRepository.save(
+                    .firstName("Isidor")
+                    .lastName("Teurteur")
+                    .email("ll@l.com")
+                    .password(passwordEncoder.encode("bib"))
+                    .build()
+            );
+        }
+        if (userAppRepository.findUserAppByEmail("lll@l.com").isEmpty()) {
+            preposeRepository.save(
                 Professor.builder()
-                        .firstName("Chandeuse")
-                        .lastName("Lixor")
-                        .email("lll@l.com")
-                        .password(passwordEncoder.encode("bib"))
-                        .build()
-        );
+                    .firstName("Chandeuse")
+                    .lastName("Lixor")
+                    .matricule("PROF-001")
+                    .email("lll@l.com")
+                    .password(passwordEncoder.encode("bib"))
+                    .build()
+            );
+        }
         final Optional<User> userAppByEmail = userAppRepository.findUserAppByEmail("l@l.com");
         userAppByEmail.ifPresent(userApp -> System.out.println("user " + userAppByEmail));
 
