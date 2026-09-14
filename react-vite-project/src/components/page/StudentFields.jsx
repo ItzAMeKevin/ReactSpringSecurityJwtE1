bd6915ed788ececf3063b79889977c90f5b8188b
prodiigiio <pvalitiana@gmail.com>
2026-09-14 11:07:39 -0400

    cleanup of StudentFields component

--- react-vite-project/src/components/page/StudentFields.jsx
+++ react-vite-project/src/components/page/StudentFields.jsx
@@ -1,14 +1,14 @@
 const StudentFields = ({ errors, programmes }) => {
     return (
         <>
-            <div>
+            <div className="flex flex-col">
                 <label>Matricule</label>
                 <input type="text" name="matricule"/>
                 {errors.matricule && <span className="text-red-500 text-sm">{errors.matricule}</span>}
             </div>
-            <div>
+            <div className="flex flex-col">
                 <label>Programme d'étude</label>
-                <select id={'programmes'} name="programme">
+                <select id="programmes" name="programme">
                     <option value="">Sélectionnez un programme</option>
                     {programmes.map((programme) => (
                         <option value={programme} key={programme}>{programme}</option>
