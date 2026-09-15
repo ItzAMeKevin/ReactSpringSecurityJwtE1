90058dffc44b61616ebbebb85a0538826812bd98
prodiigiio <pvalitiana@gmail.com>
2026-09-15 13:22:25 -0400

    feat: display inscription form errors inline within fields

--- react-vite-project/src/components/page/inscription/UserFields.jsx
+++ react-vite-project/src/components/page/inscription/UserFields.jsx
@@ -1,34 +1,13 @@
+import Field from "../Field";
 import StudentFields from "../StudentFields";
-
-const labelClass = "mb-1 font-medium text-[#2b1a12]";
-const inputClass = "rounded-md border border-[#5c4432] bg-white px-3 py-2 text-[#2b1a12] focus:outline-none focus:ring-2 focus:ring-[#4b1113]";
 
 const UserFields = ({ role, errors, programmes }) => (
   <>
-    <div className="flex flex-col">
-      <label className={labelClass}>Nom</label>
-      <input type="text" name="nom" className={inputClass}/>
-      {errors.nom && <span className="text-red-700 text-sm text-center">{errors.nom}</span>}
-    </div>
-    <div className="flex flex-col">
-      <label className={labelClass}>Prenom</label>
-      <input type="text" name="prenom" className={inputClass}/>
-      {errors.prenom && <span className="text-red-700 text-sm text-center">{errors.prenom}</span>}
-    </div>
-    <div className="flex flex-col">
-      <label className={labelClass}>Courriel</label>
-      <input type="email" name="courriel" className={inputClass}/>
-      {errors.courriel && <span className="text-red-700 text-sm text-center">{errors.courriel}</span>}
-    </div>
-    <div className="flex flex-col">
-      <label className={labelClass}>Mot de passe</label>
-      <input type="password" name="motDePasse" className={inputClass}/>
-    </div>
-    <div className="flex flex-col">
-      <label className={labelClass}>Confirmation de mot de passe</label>
-      <input type="password" name="confirmation" className={inputClass}/>
-      {errors.confirmation && <span className="text-red-700 text-sm text-center">{errors.confirmation}</span>}
-    </div>
+    <Field label="Nom" name="nom" error={errors.nom} />
+    <Field label="Prenom" name="prenom" error={errors.prenom} />
+    <Field label="Courriel" name="courriel" type="email" error={errors.courriel} />
+    <Field label="Mot de passe" name="motDePasse" type="password" />
+    <Field label="Confirmation de mot de passe" name="confirmation" type="password" error={errors.confirmation} />
     {role === "student" && <StudentFields errors={errors} programmes={programmes} />}
   </>
 );
