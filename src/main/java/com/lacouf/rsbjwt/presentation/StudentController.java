44604089b4d72923623e519b5eb2f73b00a3e1b1
luis Quezada Valle <luiscqvjob@gmail.com>
2026-09-23 01:31:08 -0400

    refactor nom de class StudentController

--- src/main/java/com/lacouf/rsbjwt/presentation/StudentController.java
+++ src/main/java/com/lacouf/rsbjwt/presentation/StudentController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/student-cv")
-public class StudentCvController {
+public class StudentController {
     private final StudentCvService studentCvService;
 
     @PostMapping
